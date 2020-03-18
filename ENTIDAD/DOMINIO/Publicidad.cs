a632e80b2ff651a05ae9e515ce47e55104739501
Karo <karoll.university@gmail.com>
2020-03-18 11:27:47 -0600

    Configuracion en la capa de entidades

--- ENTIDAD/DOMINIO/Publicidad.cs
+++ ENTIDAD/DOMINIO/Publicidad.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ENTIDAD.DOMINIO
 {
   public  class Publicidad {
+
+        [DisplayFormat(NullDisplayText = "Sin Respuesta")]
+        [Display(Name = "Código")]
+        [Key]
+        public int codigoHabitacion { get; set; }
     }
 }
